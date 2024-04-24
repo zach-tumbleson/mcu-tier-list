b1b3fd3606e82f895252e7156b590eb79904eebd
Nathan Molby <nathan.molby@willowtreeapps.com>
2024-04-24 14:32:44 -0400

    final tier list view

--- MCU Tier List/MCU Tier List/View/MovieFinalTierListView.swift
+++ MCU Tier List/MCU Tier List/View/MovieFinalTierListView.swift
@@ -15,20 +15,42 @@ struct MovieFinalTierListView: View {
     }
     
     var body: some View {
-        ForEach(TierListElement.allCases, id: \.rawValue) { element in
-            ScrollView {
-                HStack {
-                    TierListElementView(element: element)
-                    
-                    ForEach(ratingsGrouped[element] ?? [], id: \.movie.id) { movieRating in
-                        AsyncImage(url: .init(string: movieRating.movie.coverUrl))
+        ScrollView {
+            VStack(spacing: 4) {
+                ForEach(TierListElement.allCases, id: \.rawValue) { element in
+                    ScrollView(.horizontal) {
+                        HStack {
+                            TierListElementView(element: element)
+                                .frame(width: 100, height: 100)
+                            
+                            ForEach(ratingsGrouped[element] ?? [], id: \.movie.id) { movieRating in
+                                AsyncImage(
+                                    url: .init(string: movieRating.movie.coverUrl),
+                                    content: { image in
+                                        image
+                                            .resizable()
+                                            .frame(width: 100)
+                                    },
+                                    placeholder: {
+                                        ProgressView()
+                                    })
+                            }
+                        }
                     }
+                    .frame(height: 100)
                 }
             }
+        }
+        .background {
+            Color.black
+                .ignoresSafeArea(.all)
         }
     }
 }
 
 #Preview {
-    MovieFinalTierListView(ratings: [])
+    MovieFinalTierListView(ratings: [.init(rating: .S, movie: .init(id: 1, title: "", releaseDate: "", coverUrl: "https://res.cloudinary.com/augustomarcelo/image/upload/v1658747271/mcuapi/gallery/Movies/thunderbolts/posters/1.jpg")), .init(rating: .A, movie: .init(id: 2, title: "", releaseDate: "", coverUrl: "https://res.cloudinary.com/augustomarcelo/image/upload/v1675206317/mcuapi/gallery/Movies/iron_man/posters/1.jpg"))])
 }
+
+
+
