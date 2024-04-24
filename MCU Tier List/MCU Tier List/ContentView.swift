88e6bee51caab4a9249d9bc858a91d7277753417
Zach Tumbleson <zach.tumbleson@willowtreeapps.com>
2024-04-24 13:03:18 -0400

    Create initial project

---
+++ MCU Tier List/MCU Tier List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MCU Tier List
+//
+//  Created by Zach Tumbleson on 4/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MCU Tier List/MCU Tier List/MCU_Tier_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCU_Tier_ListApp.swift
+//  MCU Tier List
+//
+//  Created by Zach Tumbleson on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MCU_Tier_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
